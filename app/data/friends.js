17ea2e96cfa34b319eb4b46086156f0f3de0c9c0
john sander <john sander>
2017-05-19 15:24:04 -0400

    paths and api updated

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friendArray = [
+{
+    "name": "JR",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAc-AAAAJDQ1ZDhhZjRhLTJkYjItNDZkZC1hYzg3LTEyYjEyM2UzNjM2Nw.jpg",
+    "scores": [
+                5,
+                2,
+                3,
+                1,
+                4,
+                5,
+                1,
+                2,
+                2,
+                4
+              ]
+},
+{
+    "name": "Bart",
+    "photo": "https://vignette1.wikia.nocookie.net/simpsons/images/4/41/Bartsimpsonskateboard.jpg/revision/latest/scale-to-width-down/159?cb=20170411164457",
+    "scores": [
+                5,
+                2,
+                3,
+                4,
+                4,
+                2,
+                4,
+                3,
+                2,
+                4
+              ]
+}];
+
+module.exports = friendArray;
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 8080;
+var path = require("path");
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
+
